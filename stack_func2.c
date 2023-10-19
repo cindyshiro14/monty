3845d797c94a6ffb855482f5aa3e39a66c56495c
ajibad-coder <meriesbells1996@gmail.com>
2023-10-19 14:27:00 -0700

    header

--- stack_func2.c
+++ stack_func2.c
@@ -53,26 +53,6 @@ void add_nodes(stack_t **stack, unsigned int line_number)
 }
 
 
-/**
- * sub_nodes - Adds the top two elements of the stack.
- * @stack: Pointer to a pointer pointing to top node of the stack.
- * @line_number: Interger representing the line number of of the opcode.
- */
-void sub_nodes(stack_t **stack, unsigned int line_number)
-{
-	int sum;
-
-	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
-
-		more_err(8, line_number, "sub");
-
-
-	(*stack) = (*stack)->next;
-	sum = (*stack)->n - (*stack)->prev->n;
-	(*stack)->n = sum;
-	free((*stack)->prev);
-	(*stack)->prev = NULL;
-}
 
 
 /**
@@ -94,4 +74,24 @@ void div_nodes(stack_t **stack, unsigned int line_number)
 	(*stack)->n = sum;
 	free((*stack)->prev);
 	(*stack)->prev = NULL;
+}
+/**
+ * sub_nodes - Adds the top two elements of the stack.
+ * @stack: Pointer to a pointer pointing to top node of the stack.
+ * @line_number: Interger representing the line number of of the opcode.
+ */
+void sub_nodes(stack_t **stack, unsigned int line_number)
+{
+        int sum;
+
+        if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+
+                more_err(8, line_number, "sub");
+
+
+        (*stack) = (*stack)->next;
+        sum = (*stack)->n - (*stack)->prev->n;
+        (*stack)->n = sum;
+        free((*stack)->prev);
+        (*stack)->prev = NULL;
 }
